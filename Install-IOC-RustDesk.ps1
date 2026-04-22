a0c13cedf06ccb1cba749f7522c972fbc2f6f75f
luanrousseau-spec <luan.rousseau@gmail.com>
2026-04-22 13:14:26 +0200

    Add files via upload

---
+++ Install-IOC-RustDesk.ps1
@@ -0,0 +1,9 @@
+﻿function Show-Progress {
+    param(
+        [int]$Percent,
+        [string]$Message
+    )
+
+    Write-Host ("[{0,3}%] {1}" -f $Percent, $Message)
+    Log ("{0}% - {1}" -f $Percent, $Message)
+}
